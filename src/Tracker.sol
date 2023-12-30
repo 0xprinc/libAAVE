e5afbc3d4b8f3cf01e047123251108074b7f7680
0xprinc <0xprinc@gmail.com>
2023-12-30 22:07:22 +0530

    Update Tracker.sol

--- src/Tracker.sol
+++ src/Tracker.sol
@@ -1,59 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "./Base64.sol";
 
 contract Tracker is ERC721, Base64 {
 
     mapping(uint256 => info) public desc;
+    address constant TARGET = 0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2; // pool.sol
 
-        struct info {
+    struct info {
         string name;                  // name that the user wants to give to their nft
-        address target;               // caller contract address
-        bytes data;                   // data to send to the target
-        uint datatype;                // 1 for uint, 2 for address, 3 for bool, 4 for string
     }
 
     constructor() ERC721("Track", "TRACK") {}
 
-    function _burn(uint256 tokenId) internal override(ERC721) {
-        super._burn(tokenId);
-    }
-
     function mint(
         address to, 
         uint256 _tokenId, 
-        string memory _name,
-        address _target,
-        bytes memory _data,
-        uint _datatype) 
+        string memory _name) 
     public {
         _safeMint(to, _tokenId);
-        setDesc(_target, _data, _tokenId, _name, _datatype);
+        setDesc(_tokenId, _name);
     }
 
-    function setDesc(address _target, bytes memory _data, uint _tokenId, string memory _name, uint _datatype) public{
+    function setDesc(uint _tokenId, string memory _name) public{
         require(ownerOf(_tokenId) == msg.sender, "You are not the owner");
-        desc[_tokenId] = info(_name, _target, _data, _datatype);
+        desc[_tokenId] = info(_name);
     }
 
     function getSvg(uint tokenId) public view returns (string memory) {
         string memory output;
         info storage description = desc[tokenId];
-        (,bytes memory data) = (description.target).staticcall(description.data);
-        if(description.datatype == 1){
-            output = uint2str(abi.decode(data, (uint)));
-        }
-        else if(description.datatype == 2){
-            output = addressToString(abi.decode(data, (address)));
-        }
-        else if(description.datatype == 3){
-            output = boolToString(abi.decode(data, (bool)));
-        }
-        else if(description.datatype == 4){
-            output = abi.decode(data,(string));
-        }
+        (,bytes memory _data) = (TARGET).staticcall(abi.encodeWithSignature("getUserAccountData(address)",ownerOf(tokenId)));
+        (uint totalCollateralBase, uint totalDebtBase, uint availableBorrowsBase, uint currentLiquidationThreshold, uint ltv, uint healthFactor) = abi.decode(_data,(uint,uint, uint, uint, uint, uint));
         string memory svg;
         svg = concatenateStrings("<svg width='250' height='400' xmlns='http://www.w3.org/2000/svg'><style>text{font-family:Arial,sans-serif;font-size:24px;fill:#fff;text-anchor:middle}</style><rect width='100%' height='100%'/><text x='125' y='150' style='font-weight:700;fill:#0af'>",description.name,"</text><text x='125' y='250' style='font-style:italic;fill:#f50'>",output,"</text></svg>");
         return svg;
@@ -71,4 +51,3 @@ contract Tracker is ERC721, Base64 {
         return string(abi.encodePacked('data:application/json;base64,', json));
     }    
 }
-
